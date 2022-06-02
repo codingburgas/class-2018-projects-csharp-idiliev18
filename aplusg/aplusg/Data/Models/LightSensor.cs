4464ebc4ad19f996e03ca95375cbdf323d86db97
idiliev18 <idiliev18@codingburgas.bg>
2022-06-02 23:12:12 +0300

    Add LightSensor class

---
+++ aplusg/aplusg/Data/Models/LightSensor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aplusg.Data.Models
+{
+    internal class LightSensor
+    {
+        public int Id { get; set; }
+        public string CurrentLightValue { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
