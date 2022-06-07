d8a52245c200da4dcc32f5275322702bde782b9b
SNKolev18 <SNKolev18@codingburgas.bg>
2022-06-07 15:24:38 +0300

    Implement IUserService
    *  Start work on UserService class
    * Create model that will be used when an authentication request is being made
    * Create model that will be used after authentication as response which will be sent to the Angular client

---
+++ api/aplusg/Data/Models/AuthResponse.cs
@@ -0,0 +1,27 @@
+﻿using aplusg.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aplusg.Data.Models
+{
+	public class AuthResponse
+	{
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Token { get; set; }
+
+
+        public AuthResponse(User user, string token)
+        {
+            Id = user.Id;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            Username = user.Username;
+            Token = token;
+        }
+    }
+}
--- api/aplusg/Services/UserService.cs
+++ api/aplusg/Services/UserService.cs
@@ -1,16 +1,39 @@
-﻿using System;
+﻿using aplusg.Data.Models;
+using aplusg.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Security.Claims;
+using System.Text;
 
 namespace aplusg.Services
 {
 	public interface IUserService
 	{
-
+		AuthResponse Authenticate(AuthRequest model);
+		IEnumerable<User> GetAll();
+		User GetById(int id);
 	}
 	public class UserService : IUserService
 	{
+		private AplusGDbContext _context;
+		private List<User> users = new List<User>();
+		public AuthResponse Authenticate(AuthRequest model)
+		{
+			throw new NotImplementedException();
+		}
 
+		public IEnumerable<User> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public User GetById(int id)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
