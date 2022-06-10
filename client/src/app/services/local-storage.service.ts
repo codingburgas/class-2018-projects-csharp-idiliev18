8ecf9ba7227614b11eb5d39dc85a895532d29e2c
idiliev18 <idiliev18@codingburgas.bg>
2022-06-10 17:01:32 +0300

    Implement LocalStorage Service
    * Implement readFromLocalStorage()
    * writeToLocalStorage()
    * deleteFromLocalStorage()

---
+++ client/src/app/services/local-storage.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+ readFromLocalStorage(key:string) {
+  if (localStorage.getItem(key))
+  {
+     return JSON.parse(localStorage.getItem(key) || '{}');
+  }
+  else {
+    return null;
+  }
+}
+
+
+ writeToLocalStorage(key:string, data:string) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
+ deleteFromLocalStorage(key:string) {
+  localStorage.removeItem(key);
+}
+}
