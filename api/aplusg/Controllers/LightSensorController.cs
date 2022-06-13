bbf7d721702e610ddbfd9c8c5aee87da64e016b4
idiliev18 <idiliev18@codingburgas.bg>
2022-06-14 02:51:43 +0300

    Fix models problem

---
+++ api/aplusg/Controllers/LightSensorController.cs
@@ -0,0 +1,54 @@
+﻿using aplusg.Data.Models;
+using aplusg.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace aplusg.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class LightSensorController : ControllerBase
+	{
+		private readonly AplusGDbContext _context;
+
+		public LightSensorController(AplusGDbContext context)
+		{
+			_context = context;
+		}
+
+		// GET: api/<LightSensorController>
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<LightSensor>>> GetAll()
+		{
+			return await _context.LightSensors.ToListAsync();
+		}
+
+		// GET api/<LightSensorController>/5
+		[HttpGet("{id}")]
+		public async Task<ActionResult<LightSensor>> Get(int id)
+		{
+			var ls = await _context.LightSensors.FindAsync(id);
+			return (ls is not null) ? ls : NotFound();
+
+		}
+
+		// POST api/<LightSensorController>
+		[HttpPost("Create")]
+		public ActionResult Post([FromBody] LightSensor lightSensor)
+		{
+			return Ok();
+		}
+
+		// DELETE api/<LightSensorController>/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+}
